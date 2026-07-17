38c1cedbb96b23d7cdd84f2b67a8fe3b8f0f0651
Tuan Ngoc <aduhacknao2004@gmail.com>
2026-07-17 21:42:10 +0700

    u

--- src/components/layout/HomeLayout.jsx
+++ src/components/layout/HomeLayout.jsx
@@ -151,10 +151,27 @@ function HomeLayout({ children }) {
     }
   }
 
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true)
+  const lastScrollY = useRef(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      if (currentScrollY > lastScrollY.current && currentScrollY > 80) {
+        setIsHeaderVisible(false)
+      } else {
+        setIsHeaderVisible(true)
+      }
+      lastScrollY.current = currentScrollY
+    }
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <div className="home-layout-container">
       {/* HEADER */}
-      <header className="home-header">
+      <header className={`home-header ${!isHeaderVisible ? 'hidden' : ''}`}>
         <div className="home-header-left">
           {/* Logo */}
           <Link to="/" className="home-brand">
